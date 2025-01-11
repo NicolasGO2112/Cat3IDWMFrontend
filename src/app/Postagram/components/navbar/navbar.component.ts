847f23120e37c2649ff0b6b309358cf27d88daa9
NicolasGO2112 <nicolas.galleguillos01@alumnos.ucn.cl>
2025-01-10 21:01:42 -0300

    feat: Readme.md

--- src/app/Postagram/components/navbar/navbar.component.ts
+++ src/app/Postagram/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private router: Router) {}
 
+  logout() {
+    localStorage.clear(); // Limpia los datos de localStorage
+    sessionStorage.clear(); // Limpia los datos de sessionStorage (opcional)
+    alert('Sesión cerrada exitosamente.'); // Opcional
+    this.router.navigate(['/loggin']); // Redirige al usuario a la página de login
+  }
 }
